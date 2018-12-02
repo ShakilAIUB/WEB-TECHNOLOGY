eaabdfc4b40c58f431b168667635d724c81219eb
Shakil Sarder <40805013+ShakilAIUB@users.noreply.github.com>
2018-12-02 19:19:38 +0600

    CSS_ASSIGNMENT

--- index.php
+++ index.php
@@ -1,45 +1,99 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Saef
- * Date: 08-Mar-16
- * Time: 9:41 AM
- */
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>CSS Assignment</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
 
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
 
+		<div class="top_menu">
+		<div class="top_menu_in">
+		<div class="menu_inside">
+		<ul>
+			<li><a href="index.php" style="border-right: 1px solid #e2e2e2;">Students</a></li></li>
+			<li><a href="index.php" style="border-right: 1px solid #e2e2e2;">Staff</a></li>
+			<li><a href="index.php" style="border-right: 1px solid #e2e2e2;">Alumni</a></li>
+			<li><a href="index.php">myHub</a></li>
+		</ul>
+		</div>
+		<div class="menu_search">
+		<input class="menu_search_box" type="text" name="Search" placeholder="Search Our Site">
+		</div>
+		</div>
+		</div>
+		
+		<!-Main Menu-!>
+		<div class="main_menu">
+		<div class="main_menu_in">
+		<div class="menu_logo">
+		<a href="index.php"><img src="logo.png" alt="University Logo"></a>
+		</div>
+		<div class="menu_item">
+		<ul>
+			<li><a href="#" >Home</a></li></li>
+			<li><a href="#" >WHy BU?</a></li>
+			<li><a href="#" >Study</a></li>
+			<li><a href="#" >Research</a></li></li>
+			<li><a href="#" >Collaborate</a></li>
+			<li><a href="#" >Global BU</a></li>
+			<li><a href="#">About</a></li>
+		</ul>
+		</div>
+		
+		</div>
+		</div>
+		
+		<div class="slider">
+		<div class="slider_menu" >
+		
+			<div class="slider_menu_content">
+				<a href="#" >Themes |</a>
+				<a href="#" >Centres |</a>
+				<a href="#" >Impact |</a>
+				<a href="#" >Environment |</a>
+				<a href="#" >Postgraduate |</a>
+				<a href="#" >Our latest research Research Chronicle |</a>
+				<a href="#" >Research Chronicle |</a>
+				<a href="#" >British Conference of Undergraduate Research |</a>
+				<a href="#">Showcasing Undergraduate Research Excellence |</a>
+			</div>	
+		</div>
+		
+		<div class="slider_content">
+		<p>World-Leading</p>
+		<span>resarch</span>
+		<br>
+		<a href=""><button class="slider_button">Read the latest Bounmouth resarch chironicle</button></a>
+		</div>
+		
+		</div>
+		
+		<div class="call_to_action">
+			<h1></h1>
+		<input type="text" name="Search" placeholder="Search Our Course">
+		</div>
 
-?>
-<html>
-<head>
-    <link rel="stylesheet" href="css.css">
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body class="container-fluid">
-<h1>Assignment Uploading System</h1>
-<hr>
-<h3>Login Here or <a href="register.php">Click Here for Registration</a></h3><br>
-        <form name="login" method="post" action="enter.php">
-
-            <div class="form-group">
-                <label for="AIUB ID">AIUB ID:</label>
-                <input type="text" class="form-control" id="un" name="un">
-            </div>
-            <div class="form-group">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control" id="pw" name="pw">
-            </div>
-            <input type="submit" value="Login" class="btn btn-default">
-            <!--<button type="submit" class="btn btn-default">Submit</button>-->
-        </form>
-
-
-
-
-
-
-
-</body>
+		<div style="width:100%; height:30px; background:black">
+		</div>
+		
+		<div class="text_content">
+		<div class="text_c">
+		<a style="text-decoration: none" href="#">Home</a>/ Research
+		<hr>
+		Our research shapes and changes the world around us, providing solutions to real-world problems and informing the education we deliver. Our students are a key part of the research we conduct, co-creating knowledge with us and playing a crucial role in everything that we do.
+		</div>
+		</div>
+		
+		<div class="box">
+		<div class="left"><img src="bike.jpg" alt="University Logo"></div>
+		<div class="center"><img src="center.jpg" alt="University Logo"></div>
+		<div class="right"><img src="bike.jpg" alt="University Logo"></div>
+		<div class="left"><img src="bike.jpg" alt="University Logo"></div>
+		<div class="right"><img src="bike.jpg" alt="University Logo"></div>
+		</div>
+    </body>
 </html>
